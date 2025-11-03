c4eac96a7154b67f7eb2f287115e561494185ddb
ThePrimeagen <2flynnman@gmail.com>
2025-11-03 13:37:07 -0800

    added DAO

---
+++ dao/ApplysDAO.js
@@ -0,0 +1,37 @@
+import mongodb from "mongodb"
+const ObjectId = mongodb.ObjectId
+
+let applys
+
+export default class ApplysDAO{
+    static async injectDB(conn){
+        if(applys){
+            return
+        }
+        try {
+            applys = await conn.db("CNPSC").collection("Applys")
+        } catch (e) {
+            console.error(`Unable to establish collection handles in ApplysDAO: ${e}`)
+        }
+    }
+
+    static async addApp(name, phone, address, city, zip, date, license, notes){
+        try {
+            const AppObject = {
+                name: name,
+                phone: phone,
+                address: address,
+                city: city,
+                zip: zip,
+                date: date,
+                license: license,
+                notes: notes
+            }
+
+            return await applys.insertOne(AppObject)
+        } catch(e){
+            console.log(`Unable to Post Applaction: ${e}`)
+            return { error : e }
+        }
+    }
+}
